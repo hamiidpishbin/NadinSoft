a9ed81689b4befbfe4dd563abfec9f744b98b6a0
Hamid Pishbin <hamidpishbin89@gmail.com>
2024-02-05 00:16:43 +0330

    Add ProductController and Add API

---
+++ NadinSoft.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NadinSoft.Application.Products.Commands;
+
+namespace NadinSoft.Web.Controllers;
+
+public class ProductController : BaseApiController
+{
+  private readonly IMapper _mapper;
+
+  public ProductController(IMapper mapper)
+  {
+    _mapper = mapper;
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Add(AddProductRequest request, CancellationToken cancellationToken)
+  {
+    var command = _mapper.Map<AddProductCommand>(request);
+    var result = await Mediator!.Send(command, cancellationToken);
+    return HandleResult(result);
+  }
+}
--- NadinSoft.Web/Program.cs
+++ NadinSoft.Web/Program.cs
@@ -1,6 +1,6 @@
 using NadinSoft.Application;
 using NadinSoft.Infrastructure;
-using NadinSoft.Web.Controllers;
+using NadinSoft.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
